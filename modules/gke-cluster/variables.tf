50b74598ecc568bca4bc2446a09e42988bd3d198
Avinash Singh <6140019+avinashsi@users.noreply.github.com>
2024-04-03 22:20:01 +0200

    making it backward compatible (#8)

--- modules/gke-cluster/variables.tf
+++ modules/gke-cluster/variables.tf
@@ -227,11 +227,21 @@ variable "enable_workload_identity" {
   type        = bool
 }
 
-variable "identity_namespace" {
+## Replaced with  on workload_pool variable ## 
+
+# variable "identity_namespace" {
+#   description = "Workload Identity Namespace. Default sets project based namespace [project_id].svc.id.goog"
+#   default     = null
+#   type        = string
+# }
+
+##workload_pool###
+variable "workload_pool" {
   description = "Workload Identity Namespace. Default sets project based namespace [project_id].svc.id.goog"
   default     = null
   type        = string
 }
+
 variable "enable_config_connector" {
   description = "Enable ConfigConnector addon (requires workload identity)"
   default     = false
